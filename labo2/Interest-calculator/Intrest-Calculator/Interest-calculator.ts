8a2e743b077b85f6f4d5cf06ebc418f9ce32d545
Mulin̕ski Piotr (s157203) <piotr.mulinski@student.ap.be>
2025-01-25 09:11:30 +0000

    interest app

--- labo2/Interest-calculator/Intrest-Calculator/Interest-calculator.ts
+++ labo2/Interest-calculator/Intrest-Calculator/Interest-calculator.ts
@@ -1,4 +1,16 @@
 import * as rl from "readline-sync"
 
-let test : string = rl.question("Hallo");
-console.log(test)
+
+const budget : number = rl.questionInt("Geef het bedraag in: ", {limitMessage:"Geef een gehel getaal in aub !"});
+const precentage : number = rl.questionFloat("Geeff het interest precentage in: ", {limitMessage:"Geef een comma getal in aub! "});
+
+
+const oneYear : number = budget  * ((1 + (precentage/100)));
+const twoYear : number = budget  * ((1 + (precentage/100)) ** 2);
+const fiveYear : number = budget  * ((1 + (precentage/100)) ** 5);
+
+console.log(`na 1 jaar heb je : ${oneYear.toFixed(2)} euro.`)
+console.log(`na 2 jaar heb je : ${twoYear.toFixed(2)} euro.`)
+console.log(`na 5 jaar heb je : ${fiveYear.toFixed(2)} euro.`)
+
+
